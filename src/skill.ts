d39b52b05f5e3e155d009a8de599f9a22dfc3e1a
Jesse Hutchison <j.w.hutchison@live.com>
2019-07-31 10:41:41 -0400

    Skill costs / advances done

--- src/skill.ts
+++ src/skill.ts
@@ -8,4 +8,49 @@ export class Skill {
     this.CharacteristicType = type;
     this.IsAdvanced = isAdvanced;
   }
+
+  Advance(currentXp: number): number {
+    let remainingXp = currentXp - this.GetAdvanceCost();
+    if (remainingXp >= 0) {
+      this.Advances += 1;
+      return remainingXp;
+    }
+    else {
+      return currentXp;
+    }
+  }
+
+  GetAdvanceCost(): number {
+    let nextAdvance = this.Advances + 1;
+    if (nextAdvance <= 5)
+      return 10;
+    else if (nextAdvance <= 10)
+      return 15;
+    else if (nextAdvance <= 15)
+      return 20;
+    else if (nextAdvance <= 20)
+      return 30;
+    else if (nextAdvance <= 25)
+      return 40;
+    else if (nextAdvance <= 30)
+      return 60;
+    else if (nextAdvance <= 35)
+      return 80;
+    else if (nextAdvance <= 40)
+      return 110;
+    else if (nextAdvance <= 45)
+      return 140;
+    else if (nextAdvance <= 50)
+      return 180;
+    else if (nextAdvance <= 55)
+      return 220;
+    else if (nextAdvance <= 60)
+      return 270;
+    else if (nextAdvance <= 65)
+      return 320;
+    else if (nextAdvance <= 70)
+      return 380;
+    else
+      return 440;
+  }
 }
--- test/unit/skill.spec.ts
+++ test/unit/skill.spec.ts
@@ -29,13 +29,50 @@ describe('Skill - ', () => {
   });
 
   it('has a advances', () => {    
-    skill = new Skill("Athletics", CharacteristicType.Agility, false);
     expect(skill.Advances).toEqual(0);
   });
+  
+  it('it can be advanced', () => {
+    expect(skill.Advances).toEqual(0);
+    skill.Advance(10);
+    expect(skill.Advances).toEqual(1);
+  });
 
-  // it('can be advanced', () => {    
-  //   skill = new Skill("Athletics", CharacteristicType.Agility, false);
-  //   skill.
-  //   expect(skill.Advances).toEqual(0);
-  // });
+  it('it can return the cost to advance', () => {
+    expect(skill.Advances).toEqual(0);
+    expect(skill.GetAdvanceCost()).toEqual(10);
+    skill.Advances = 5;
+    expect(skill.GetAdvanceCost()).toEqual(15);
+    skill.Advances = 10;
+    expect(skill.GetAdvanceCost()).toEqual(20);
+    skill.Advances = 15;
+    expect(skill.GetAdvanceCost()).toEqual(30);
+    skill.Advances = 20;
+    expect(skill.GetAdvanceCost()).toEqual(40);
+    skill.Advances = 25;
+    expect(skill.GetAdvanceCost()).toEqual(60);
+    skill.Advances = 30;
+    expect(skill.GetAdvanceCost()).toEqual(80);
+    skill.Advances = 35;
+    expect(skill.GetAdvanceCost()).toEqual(110);
+    skill.Advances = 40;
+    expect(skill.GetAdvanceCost()).toEqual(140);
+    skill.Advances = 45;
+    expect(skill.GetAdvanceCost()).toEqual(180);
+    skill.Advances = 50;
+    expect(skill.GetAdvanceCost()).toEqual(220);
+    skill.Advances = 55;
+    expect(skill.GetAdvanceCost()).toEqual(270);
+    skill.Advances = 60;
+    expect(skill.GetAdvanceCost()).toEqual(320);
+    skill.Advances = 65;
+    expect(skill.GetAdvanceCost()).toEqual(380);
+    skill.Advances = 70;
+    expect(skill.GetAdvanceCost()).toEqual(440);
+  });
+
+  it('advancement takes in a number of xp, and returns the remainder after advancing', () => {
+    expect(skill.Advances).toEqual(0);
+    expect(skill.Advance(11)).toEqual(1);
+  });
 });
