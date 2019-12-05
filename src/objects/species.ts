ffb1ec863304f6a6e16dc0ef8bad1c3b3a20c13b
Jesse Hutchison <j.w.hutchison@live.com>
2019-12-05 08:56:33 -0500

    Update species.ts

--- src/objects/species.ts
+++ src/objects/species.ts
@@ -73,8 +73,31 @@ export class Dwarf extends Species {
     this.AvailableCareers.push(new SpeciesClassCareer(new Spy(),43,43));
     this.AvailableCareers.push(new SpeciesClassCareer(new Warden(),44,45));
     this.AvailableCareers.push(new SpeciesClassCareer(new Bailiff(),46,47));
-    
-    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),48,100));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Hunter(),48,49));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Miner(),50,54));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Scout(),55,55));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Villager(),56,56));
+    this.AvailableCareers.push(new SpeciesClassCareer(new BountyHunter(),57,60));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Coachman(),61,61));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Entertainer(),62,63));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Messenger(),64,65));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Pedlar(),66,67));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Boatman(),68,69));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Huffer(),70,70));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Riverwoman(),71,72));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Seaman(),73,73));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Smuggler(),74,75));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Stevedore(),76,77));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Wrecker(),78,78));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Fence(),79,79));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),80,82));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Racketeer(),83,83));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Thief(),84,84));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Guard(),85,87));
+    this.AvailableCareers.push(new SpeciesClassCareer(new PitFighter(),88,90));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Protagonist(),91,93));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Soldier(),94,96));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Slayer(),97,100));
     this.Skills = [
       SkillLibrary.ConsumeAlcohol,
       SkillLibrary.Cool,
@@ -135,8 +158,34 @@ export class Halfling extends Species {
     this.AvailableCareers.push(new SpeciesClassCareer(new Spy(),44,44));
     this.AvailableCareers.push(new SpeciesClassCareer(new Warden(),45,46));
     this.AvailableCareers.push(new SpeciesClassCareer(new Bailiff(),47,47));
-
-    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),48,100));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Herbalist(),48,50));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Hunter(),51,52));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Miner(),53,53));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Scout(),54,54));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Villager(),55,57));
+    this.AvailableCareers.push(new SpeciesClassCareer(new BountyHunter(),58,58));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Coachman(),59,60));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Entertainer(),61,63));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Messenger(),64,65));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Pedlar(),66,67));
+    this.AvailableCareers.push(new SpeciesClassCareer(new RoadWarden(),68,68));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Boatman(),69,69));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Huffer(),70,70));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Riverwarden(),71,71));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Riverwoman(),72,74));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Seaman(),75,75));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Smuggler(),76,79));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Stevedore(),80,82));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Bawd(),83,85));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Charlatan(),86,86));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Fence(),87,87));
+    this.AvailableCareers.push(new SpeciesClassCareer(new GraveRobber(),88,88));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),89,89));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Racketeer(),90,90));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Thief(),91,94));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Guard(),95,96));
+    this.AvailableCareers.push(new SpeciesClassCareer(new PitFighter(),97,97));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Soldier(),98,100));
     this.Skills = [
       SkillLibrary.Charm,
       SkillLibrary.ConsumeAlcohol,
@@ -192,13 +241,24 @@ export class HighElf extends Species {
     this.AvailableCareers.push(new SpeciesClassCareer(new Noble(),38,40));
     this.AvailableCareers.push(new SpeciesClassCareer(new Spy(),41,43));
     this.AvailableCareers.push(new SpeciesClassCareer(new Warden(),44,45));
-    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),46,47));
-    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),48,50));
-    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),51,56));
-    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),57,59));
-    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),60,62));
-
-    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),63,100));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Herbalist(),46,47));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Hunter(),48,50));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Scout(),51,56));
+    this.AvailableCareers.push(new SpeciesClassCareer(new BountyHunter(),57,59));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Entertainer(),60,62));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Messenger(),63,63));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Boatman(),64,64));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Seaman(),65,79));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Smuggler(),80,80));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Bawd(),81,82));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Charlatan(),83,85));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),86,88));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Cavalryman(),89,92));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Guard(),93,94));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Knight(),95,95));
+    this.AvailableCareers.push(new SpeciesClassCareer(new PitFighter(),96,97));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Protagonist(),98,98));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Soldier(),99,100));
     this.Skills = [
       SkillLibrary.Cool,
       SkillLibrary.Entertain_Singing,
@@ -249,9 +309,44 @@ export class Human extends Species {
     this.AvailableCareers.push(new SpeciesClassCareer(new Spy(),36,36));
     this.AvailableCareers.push(new SpeciesClassCareer(new Warden(),37,37));
     this.AvailableCareers.push(new SpeciesClassCareer(new Bailiff(),38,38));
-    this.AvailableCareers.push(new SpeciesClassCareer(new HedgeWitch(),39,39));
-
-    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),40,100));
+    this.AvailableCareers.push(new SpeciesClassCareer(new HedgeWitch(),39,39));    
+    this.AvailableCareers.push(new SpeciesClassCareer(new Herbalist(),40,40));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Hunter(),41,42));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Miner(),43,43));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Mystic(),44,44));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Scout(),45,45));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Villager(),46,50));
+    this.AvailableCareers.push(new SpeciesClassCareer(new BountyHunter(),51,51));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Coachman(),52,52));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Entertainer(),53,54));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Flagellant(),55,56));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Messenger(),57,57));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Pedlar(),58,58));
+    this.AvailableCareers.push(new SpeciesClassCareer(new RoadWarden(),59,59));
+    this.AvailableCareers.push(new SpeciesClassCareer(new WitchHunter(),60,60));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Boatman(),61,62));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Huffer(),63,63));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Riverwarden(),64,65));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Riverwoman(),66,68));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Seaman(),69,70));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Smuggler(),71,71));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Stevedore(),72,73));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Wrecker(),74,74));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Bawd(),75,76));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Charlatan(),77,77));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Fence(),78,78));
+    this.AvailableCareers.push(new SpeciesClassCareer(new GraveRobber(),79,79));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),80,83));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Racketeer(),84,84));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Thief(),85,87));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Witch(),88,88));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Cavalryman(),89,90));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Guard(),91,92));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Knight(),93,93));
+    this.AvailableCareers.push(new SpeciesClassCareer(new PitFighter(),94,94));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Protagonist(),95,95));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Soldier(),96,99));
+    this.AvailableCareers.push(new SpeciesClassCareer(new WarriorPriest(),100,100));
     this.Skills = [
       SkillLibrary.AnimalCare,
       SkillLibrary.Charm,
@@ -293,9 +388,21 @@ export class WoodElf extends Species {
     this.AvailableCareers.push(new SpeciesClassCareer(new Artist(),15,18));
     this.AvailableCareers.push(new SpeciesClassCareer(new Envoy(),19,25));
     this.AvailableCareers.push(new SpeciesClassCareer(new Noble(),26,31));
-    this.AvailableCareers.push(new SpeciesClassCareer(new Spy(),32,35));   
-
-    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),36,100));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Spy(),32,35));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Herbalist(),36,42));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Hunter(),43,52));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Mystic(),53,57));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Scout(),58,68));
+    this.AvailableCareers.push(new SpeciesClassCareer(new BountyHunter(),69,70));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Entertainer(),71,75));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Messenger(),76,78));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Wrecker(),79,79));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Outlaw(),80,85));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Cavalryman(),86,90));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Guard(),91,92));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Knight(),93,94));
+    this.AvailableCareers.push(new SpeciesClassCareer(new PitFighter(),95,96));
+    this.AvailableCareers.push(new SpeciesClassCareer(new Soldier(),97,100));
     this.Skills = [
       SkillLibrary.Athletics,
       SkillLibrary.Climb,
