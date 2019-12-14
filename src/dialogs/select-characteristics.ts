2b92deada5985f9589d5ff2997c37ac01034bdd2
Jesse Hutchison <j.w.hutchison@live.com>
2019-12-14 17:43:55 -0500

    Swapping characteristics working

--- src/dialogs/select-characteristics.ts
+++ src/dialogs/select-characteristics.ts
@@ -5,25 +5,50 @@ import { Character } from '../objects/character';
 
 @autoinject()
 export class SelectCharacteristics {
-  character:Character;
+  character: Character;
   swapTargetCharacteristic: Characteristic;
-  constructor(public dialogController: DialogController) {    
+  constructor(public dialogController: DialogController) {
+
   }
 
-  activate(character: Character) {    
-    this.character = character;    
+  activate(character: Character) {
+    this.character = character;
 
     this.character.rollCharacteristics();
   }
 
-   selectType(event:any, characteristic: Characteristic){
-     
-     //this.character.selectCareer(this.selectedType);
-   }
+  selectType(event: any, characteristic: Characteristic) {
 
-   swap(){
-     
-   }
+    //this.character.selectCareer(this.selectedType);
+  }
+
+  swap() {
+
+  }
+
+  list: HTMLElement;
+  itemDraggedTo: Characteristic;
+  itemDragged: Characteristic;
+
+  drag(characteristic: Characteristic) {
+    this.itemDragged = characteristic;
+    if (!this.list.classList.contains('dragging')) {
+      this.list.classList.add('dragging');
+    }
+    return true;
+  }
+
+  drop() {
+    debugger;
+    this.itemDragged.SwapStartingScores(this.itemDraggedTo);
+    if (this.list.classList.contains('dragging')) {
+      this.list.classList.remove('dragging');
+    }
+    return true;
+  }
+
+  dragover(characteristic: Characteristic) {
+    this.itemDraggedTo = characteristic;
+    return true;
+  }
 }
-
-
